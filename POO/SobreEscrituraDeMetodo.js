a858c5cfc0eae24ceb13c0bae7c2added1fad920
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-07 13:18:30 -0300

    Objeto empleado agregado

--- POO/SobreEscrituraDeMetodo.js
+++ POO/SobreEscrituraDeMetodo.js
@@ -1,5 +1,4 @@
 //SOBRE ESCRITURA DE METODO
-
 class Empleado {
 
     constructor(nombre, sueldo) {
@@ -21,26 +20,33 @@ class Empleado {
     obtenerDetalle() {
         return `Empleado: ${this._nombre}, Sueldo: ${this._sueldo} `
     }
-   
-
 }
-
 class Gerente extends Empleado {
 
     constructor(nombre, sueldo, departamento) {
         super(nombre, sueldo);
         this._departamento = departamento;
-
     }
-    get departamento() { return this._departamento; }
-    set departamento(departamento){this._departamento = departamento;}
+    get departamento() {
+        return this._departamento;
+    }
+    set departamento(departamento) {
+        this._departamento = departamento;
+    }
     obtenerDetalle() {
         return `Gerente:${super.obtenerDetalle()}, Departamento: ${this._departamento} `;
     }
 }
-
-let gerente1 = new Gerente(`Carlos`,800,`Finanzas`);
+//EL OBJETO GERENTE UNICAMENTE IMPRIME DATOS DEL OBJETO GERENTE.
+let gerente1 = new Gerente(`Carlos`, 800, `Finanzas`);
 console.log(gerente1.obtenerDetalle());
 
-let gerente2 = new Gerente(`Carla`,800,`Sistemas`);
+let gerente2 = new Gerente(`Carla`, 800, `Sistemas`);
 console.log(gerente2.obtenerDetalle());
+
+//EL OBJETO EMPLEADO UNICAMENTE IMPRIME DATOS DEL OBJETO EMPLEADO.
+let empleado1 = new Empleado(`Austin`, 1200);
+console.log(empleado1.obtenerDetalle());
+
+let empleado2 = new Empleado(`Alejandro`, 1000);
+console.log(empleado2.obtenerDetalle());
