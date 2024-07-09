3a48b9bdc342c8b39b06391911d5a646b25806d0
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-09 12:44:10 -0300

    Errores

--- POO/Errores.js
+++ POO/Errores.js
@@ -19,3 +19,20 @@ obj.method(); // Error en tiempo de ejecución: 'method' no está definido en ob
 //Manejo de Errores
 //En JavaScript, se utiliza la estructura try...catch...finally para manejar errores de manera controlada:
 
+try {
+    // Código que podría lanzar un error
+    let result = someFunction();
+} catch (error) {
+    // Código para manejar el error
+    console.error('Ha ocurrido un error:', error.message);
+} finally {
+    // Código opcional que se ejecuta siempre, con o sin error
+    console.log('Finalizando operación');
+}
+
+/*
+try: Contiene el bloque de código que puede lanzar una excepción.
+catch: Captura la excepción lanzada en el bloque try. error es la variable que contiene el objeto de error.
+finally: Opcionalmente, se ejecuta siempre después de try y catch, ya sea que haya ocurrido un error o no. 
+Se utiliza comúnmente para limpiar recursos o finalizar operaciones.
+*/
