ef0d7be2169834c42b73ff54ec061b192e946ad6
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-09 12:43:35 -0300

    Errores

---
+++ POO/Errores.js
@@ -0,0 +1,21 @@
+//MANEJO DE ERRORES
+/*
+El manejo de errores en JavaScript es fundamental para escribir código robusto y manejar situaciones 
+inesperadas de manera controlada. Aquí te explico los conceptos básicos:
+
+Tipos de Errores en JavaScript
+Errores de Sintaxis: Ocurren cuando la estructura del código no es válida y el intérprete no puede 
+interpretarlo correctamente.
+*/
+
+/*
+Errores de Tiempo de Ejecución (Runtime Errors): Estos errores ocurren durante la ejecución del programa
+ y pueden ser causados por entradas incorrectas, manipulación de objetos inexistentes, entre otros.
+*/
+let obj = {};
+obj.method(); // Error en tiempo de ejecución: 'method' no está definido en obj
+
+
+//Manejo de Errores
+//En JavaScript, se utiliza la estructura try...catch...finally para manejar errores de manera controlada:
+
