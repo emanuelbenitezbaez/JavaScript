9260b09f9442d17b1bbce65da56ba9e89ac405dd
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-17 18:01:57 -0300

    Peticiones post

--- Axios/index.js
+++ Axios/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//METODO GET
 function getUsers() {
     // Devolver la promesa directamente
     return axios.get('https://jsonplaceholder.typicode.com/users', {
@@ -11,6 +11,24 @@ function getUsers() {
 }
 
 getUsers()
+    .then((response) => {
+        console.log(response.data); // Acceder a la propiedad `data` del objeto `response`
+    }).catch((error) => {
+        console.log(error);
+    });
+
+//METODO POST
+function createUsers() {
+    // Devolver la promesa directamente
+    return axios.post('https://reqres.in/api/register', {
+
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+
+    });
+}
+
+createUsers()
     .then((response) => {
         console.log(response.data); // Acceder a la propiedad `data` del objeto `response`
     }).catch((error) => {
