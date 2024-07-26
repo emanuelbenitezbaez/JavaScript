baa732165f6e2bdb876afa6346e1c5347c958e57
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-26 15:59:00 -0300

    Metodos filter y reduce

--- Arrays/metodosArrays.js
+++ Arrays/metodosArrays.js
@@ -3,3 +3,12 @@
 
 const numeros = [1, 2, 3];
 const numerosDoblados = numeros.map(numero => numero * 2); // numerosDoblados es [2, 4, 6]
+
+//filter(): Crea un nuevo array con todos los elementos que cumplan una condición determinada.
+const edades = [25, 18, 32, 16];
+const adultos = edades.filter(edad => edad >= 18); // adultos es [25, 32]
+
+//reduce(): Reduce todos los elementos de un array a un único valor.
+
+const numeroses = [1, 2, 3, 4];
+const suma = numeroses.reduce((total, numero) => total + numero, 0); // suma es 10
