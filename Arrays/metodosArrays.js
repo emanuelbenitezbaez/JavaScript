bd3954b417dfc0c8e8625f3b1184cf4ba30ac66d
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-26 15:55:44 -0300

    Arrays, metodos de transformacion de arrays

---
+++ Arrays/metodosArrays.js
@@ -0,0 +1,5 @@
+//Métodos para transformar arrays
+//map(): Crea un nuevo array aplicando una función a cada elemento del array original.
+
+const numeros = [1, 2, 3];
+const numerosDoblados = numeros.map(numero => numero * 2); // numerosDoblados es [2, 4, 6]
