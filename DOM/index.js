f8e10e7689a5ea65b628dd24ecce54f69d3ce46d
Emanuel <emanuelbenitezbaez@gmail.com>
2024-07-29 16:09:47 -0300

    getElementByClassName

--- DOM/index.js
+++ DOM/index.js
@@ -36,8 +36,8 @@ Añadir o reemplazar texto sin afectar el formato HTML existente.
 let titulo = document.getElementById("titulo");
 let subtitulo = document.getElementById("subtitulo");
 
-console.log("Título",titulo.innerHTML);
-console.log("Subtítulo",subtitulo.innerHTML);   
+console.log("Título", titulo.innerHTML);
+console.log("Subtítulo", subtitulo.innerHTML);
 
 
 //Una vez que has obtenido una referencia a un elemento HTML usando document.getElementById(), 
@@ -66,4 +66,25 @@ for (let i = 0; i < parrafos.length; i++) {
     // Imprimimos el índice del párrafo y su contenido HTML
     console.log(`Indice : ${i} - ${parrafos[i].innerHTML}`);
 }
-     
+/**
+ 
+ 
+¿Qué hace getElementsByClassName()?
+
+Esta función de JavaScript te permite seleccionar y manipular elementos HTML que comparten una clase específica. 
+Es decir, si tienes varios elementos con la misma clase, puedes usar esta función para agruparlos y
+ realizar acciones sobre ellos de manera conjunta.
+ */
+
+// Seleccionamos todos los elementos con la clase "destacado"
+const elementosDestacados = document.getElementsByClassName("destacado");
+
+// Recorremos cada elemento usando un for
+for (let i = 0; i < elementosDestacados.length; i++) {
+    // Imprimimos el índice del elemento y su contenido HTML
+    console.log(`Elemento destacado : ${i} - ${elementosDestacados[i].innerHTML}`); 
+}
+//Tambien podemos usar for of :
+for (let elemento of elementosDestacados) {
+    console.log(elemento.innerHTML);
+}
