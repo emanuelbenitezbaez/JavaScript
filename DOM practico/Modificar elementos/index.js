b54b4a4e824e35bf252c86871b58777f5d6c99f7
Emanuel <emanuelbenitezbaez@gmail.com>
2024-08-01 12:09:02 -0300

    getAttribute y setAttribute

---
+++ DOM practico/Modificar elementos/index.js
@@ -0,0 +1,38 @@
+
+const wrapper = document.querySelector('#wrapper');
+const title = document.querySelector('.title');
+const anchor = document.querySelector('.link.bold');
+console.log(anchor);
+const input = document.querySelector('[type="number"]');
+console.log(input);
+
+//Permite modificar el dom , tener cuidado al usar innerHTML porque se puede introducir codigo malicioso.
+//Para modificar el texto usar otro metodo tipo textcontent.
+
+wrapper.innerHTML = `
+<h2>He sobreescrito el DOM</h2>
+
+`
+
+    
+//Get atribute. para obtener y modificar atributos que no estan como propiedades de los elmentos.
+//Si no existe el atributo, retorna null.
+console.log(anchor.getAttribute("href"));
+
+// Set atribute . Permite agregar atributos.
+//Este metodo tiene 2 parametros. El primero es el elemento a sobreescribir.
+//El segundo parametro es el valor que va a recibir.
+
+console.log(title.setAttribute("title", "encabezado"));
+
+//////////////
+//SET ATRIBUTE PERMITE AGREGAR UN NUEVO ATRIBUTO O REEMPLAZAR EL ANTERIRO.
+let imagen = document.querySelector('img');
+
+imagen.src = "https://images7.alphacoders.com/943/943484.jpg"
+
+imagen.setAttribute("alt","imagen-spiderman");
+imagen.setAttribute("class","img");
+imagen.setAttribute("id","img-spiderman");
+
+
