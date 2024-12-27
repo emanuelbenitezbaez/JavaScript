4bc8d28d469dffd784cdaa88f711f9159bbca6af
Emanuel Benitez Baez <emanuelbenitezbaez@gmail.com>
2024-12-27 12:21:29 -0300

    js for react!

---
+++ JavaScript para react/funciones.js
@@ -0,0 +1,15 @@
+function sumar ( a ,b) {
+    return a + b;
+}
+
+function restar ( a ,b) {
+    return a - b;
+}
+function hello (name) {
+    console.log("Hello" + name);
+}
+
+console.log(sumar(2,3)); // 5
+console.log(restar(2,3)); // -1
+
+console.log(hello(" Emanuel")); // Hello Juan
